769c6b91abf75a3b094f5bbe48a7f6d30173fd65
Liru <theliru@gmail.com>
2014-04-01 00:47:59 -0400

    Basic working version.

---
+++ bot.rb
@@ -0,0 +1,15 @@
+require 'cinch'
+require 'mumble-ruby'
+require_relative "lib/cinch/plugins/mumble.rb"
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    # add all required options here
+    c.nick = 'cinch-mumble'
+    c.server = 'irc.freenode.net'
+    c.channels = ['#cinch-bots']
+    c.plugins.plugins = [CinchMumble] # optionally add more plugins
+  end
+end
+
+bot.start
--- lib/cinch/plugins/mumble.rb
+++ lib/cinch/plugins/mumble.rb
@@ -1,9 +1,53 @@
-require "cinch/mumble/version"
+require 'cinch'
 
 module Cinch
   module Plugins
-    class Mumble
+    class CinchMumble
+      include Cinch::Plugin
+      match /mumble play (.*)/, method: :play
+      match /mumble stop/, method: :stop
+      match /mumble list/, method: :list
+      match /mumble connect/, method: :connect
+
+      def play(m, file)
+        %x{mpc update}
+        m.reply "Playing..."
+        %x{mpc add #{file}; mpc play}
+      end
+
+      def stop(m)
+        m.reply 'Stopping current sound clip...'
+        %x{mpc -f %position% | head -n 1 | mpc del}
+      end
+
+      def list(m)
+        %x{mpc update}
+        m.reply 'I will PM you a list.'
+
+        tracks = %x{mpc listall}
+        m.user.send tracks.split("\n").join ', '
+      end
+
+      def execute()
+
+      end
+
+      def connect
+      end
+
+      def initialize(*args)
+        super
+        %x{mpc repeat off}
+        %x{mpc single off}
+        %x{mpc consume on}
+        @mumble = Mumble::Client.new('localhost', 64738, 'cinch-mumble')
+        @mumble.connect
+        sleep(1)
+        @mumble.join_channel 'Main Lounge'
+        @mumble.stream_raw_audio('/tmp/mumble.fifo')
+      end
 
     end
+
   end
 end
